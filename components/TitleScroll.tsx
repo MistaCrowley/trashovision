599aabd9568ab31d7f2a966a42643512637f7aa9
MistaCrowley <g14n7.5qu1d@gmail.com>
2026-04-03 01:09:40 -0400

    moppin up the slop

--- components/TitleScroll.tsx
+++ components/TitleScroll.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 'use client'
 import Marquee from "react-fast-marquee"
 import {useState, useEffect} from "react"
@@ -24,31 +24,3 @@ export default function Scrollo({texts}: ScrolloProps){
 			</Marquee>
 		</div>
 	)
-=======
-'use client'
-import Marquee from "react-fast-marquee"
-import {useState, useEffect} from "react"
-
-interface ScrolloProps{
-	texts:string;
-}
-
-export default function Scrollo({texts}: ScrolloProps){
-	const text = texts
-	const [remount, setRemount] = useState(0)
-
-	useEffect(() => {
-		setRemount(prevKey => prevKey + 1)
-	}, [texts])
-
-	return (
-		<div className="
-			w-[100%]
-		">
-			<Marquee speed={40} key={remount}>
-				<h1> {text} &nbsp;</h1>
-			</Marquee>
-		</div>
-	)
->>>>>>> 43bacb2bcb33c9ffa4befddfcac1b64b1519dc1d
-}
--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client'
 import {useState, useEffect, useRef} from "react"
 
@@ -34,41 +33,4 @@ const Player = (currentVid) => {
 	)
 }
 
-=======
-'use client'
-import {useState, useEffect, useRef} from "react"
-
-const Player = (currentVid) => {
-	const [vidurlB, setVidurlB] = useState("./video/"+currentVid.currentVid)
-	
-	useEffect(() => {
-		setVidurlB("./video/"+currentVid.currentVid)
-		console.log(`changing video to ${currentVid.currentVid}`)
-	}, [currentVid])
-
-	return (
-	<div className="
-		w-full h-full
-	"
-	>
-	    <video
-	    	key={vidurlB}
-	    	width="1920"
-	    	height="1080"
-	    	disablePictureInPicture={true}
-	    	preload="metadata"
-	    	autoPlay={true} 
-	    	loop={true}
-	    	muted={true}
-	    >
-	    <source src={vidurlB}
-	    	type="video/mp4"
-	    	/>
-	    	browsy no supporty
-	    </video>
-	</div>
-	)
-}
-
->>>>>>> 43bacb2bcb33c9ffa4befddfcac1b64b1519dc1d
 export default Player 
